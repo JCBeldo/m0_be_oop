d82f8fd2ef016ff8edf08aa1d571bbbde598340d
Julian Beldotti <julian.beldotti@gmail.com>
2023-01-05 18:30:27 -0800

    Solve it

--- oop.rb
+++ oop.rb
@@ -2,7 +2,32 @@
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+# class Unicorn
+#     def initialize(name, string)
+#         @name = name
+#         @colour = "silver"
+#         @string = string
+#     end
+#     def say
+#         "*~* #{@string} *~*"
+#     end
+# end
+# uni1 = Unicorn.new("Geoff", "Anything")
+# p uni1.say
 
+class Unicorn
+    def initialize(name)
+        @name = name
+        @colour = "silver"
+    end
+    def say(anything)
+        "*~* #{anything} *~*"
+    end
+end
+uni1 = Unicorn.new("Geoff")
+p uni1.say("Rainbow")
+p uni1.say("Howdy")
+        
 
 
 #  Write a class called Vampire
@@ -10,7 +35,19 @@
 #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
-
+class Vampire
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true
+    end
+    def drink
+        @thirsty = false
+    end
+end
+vam = Vampire.new("Vlad")
+p vam.drink
+p vam
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -18,6 +55,33 @@
 #  it should have a dynamic color attribute (string)
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+class Dragon
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+        @count = 0
+    end
+    def eat
+        # @count.to_i.next
+        @count += 1
+        if @count >= 4
+            @is_hungry = false
+        end
+    end
+    def hungry?
+        @is_hungry
+    end
+end
+drag = Dragon.new("Draco", "Julian", "green")
+drag.eat
+p drag
+drag.eat
+drag.eat
+drag.eat
+p drag.hungry?
+p drag
 
 
 #  Write a Hobbit class
@@ -28,3 +92,36 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    def initialize(name, disposition)
+        @name = name
+        @disposition = disposition
+        @age = 0
+        @is_adult = false
+        @is_old = false
+        @has_ring = has_ring
+    end
+    def celebrate_birthday
+        @age += 1
+        if @age >= 33
+            @is_adult = true
+        end
+        if @age >= 101
+            @is_old = true
+        end
+    end 
+    def has_ring
+        if @name == "Frodo" || @name == "Smeagle"
+            @has_ring = true
+        end
+    end
+end
+
+hob = Hobbit.new("Smeagle", "obsessed")
+
+
+102.times do 
+    hob.celebrate_birthday
+end
+p hob
